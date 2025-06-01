8ad9218fa01bec07c3206fae058c7a35b20e75f6
Ronan <ronangleeson20@gmail.com>
2025-06-01 14:26:11 +0000

    add swiper to home page

--- spg/src/app/pages/home/home.component.ts
+++ spg/src/app/pages/home/home.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgFor } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+
+register(); // Register Swiper custom elements
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgFor],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('swiper') swiperRef!: ElementRef;
 
+  // List all hero images here:
+  heroImages = [
+    { src: 'assets/heroes/hero1.jpg', alt: 'Artwork 1' },
+    { src: 'assets/heroes/hero2.jpg', alt: 'Artwork 2' }
+  ];
+
+  ngAfterViewInit() {
+    const swiperEl = this.swiperRef?.nativeElement;
+    if (swiperEl) {
+      const swiperParams = {
+        slidesPerView: 1,
+        loop: true,
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true
+        },
+        autoplay: {
+          delay: 4000,
+          disableOnInteraction: false,
+        },
+        injectStyles: [
+          `
+          .swiper-slide {
+            height: 100%;
+            width: 100%;
+          }
+          `
+        ],
+      };
+
+      Object.assign(swiperEl, swiperParams);
+      swiperEl.initialize();
+    }
+  }
 }
