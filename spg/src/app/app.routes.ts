8f117b4a17afb5137003228d02b41fa28c3ad5df
Ronan <ronangleeson20@gmail.com>
2025-06-01 14:50:33 +0000

    add basic navigation

--- spg/src/app/app.routes.ts
+++ spg/src/app/app.routes.ts
@@ -1,26 +1,9 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('./pages/home/home.component').then(m => m.HomeComponent)
-  },
-  {
-    path: 'gallery',
-    loadComponent: () =>
-      import('./pages/gallery/gallery.component').then(m => m.GalleryComponent)
-  },
-  {
-    path: 'about',
-    loadComponent: () =>
-      import('./pages/about/about.component').then(m => m.AboutComponent)
-  },
-  {
-    path: 'contact',
-    loadComponent: () =>
-      import('./pages/contact/contact.component').then(m => m.ContactComponent)
-  },
-  // Wildcard redirect to home if no match:
+  { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+  { path: 'gallery', loadComponent: () => import('./pages/gallery/gallery.component').then(m => m.GalleryComponent) },
+  { path: 'about', loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent) },
+  { path: 'contact', loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent) },
   { path: '**', redirectTo: '' }
 ];
