7f24ee52923d0b14530921cbe8ccd4b7dc97d0ef
Ronan <ronangleeson20@gmail.com>
2025-06-01 14:44:00 +0000

    simple garllery and about page

--- spg/src/app/pages/gallery/gallery.component.ts
+++ spg/src/app/pages/gallery/gallery.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+interface GalleryImage {
+  thumb: string;
+  full: string;
+  alt: string;
+}
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './gallery.component.html',
-  styleUrl: './gallery.component.scss'
+  styleUrls: ['./gallery.component.scss']
 })
-export class GalleryComponent {
+export class GalleryComponent implements OnInit {
+  images: GalleryImage[] = [];
+  selectedImage: GalleryImage | null = null;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // Load the JSON manifest
+    this.http
+      .get<GalleryImage[]>('assets/gallery/images.json')
+      .subscribe((data) => {
+        this.images = data;
+      });
+  }
+
+  openImage(image: GalleryImage): void {
+    this.selectedImage = image;
+  }
+
+  closeImage(): void {
+    this.selectedImage = null;
+  }
 }
